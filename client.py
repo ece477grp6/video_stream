d67de387a1c75c3b27a4afae5e16a2319d20dac5
Qianli Ma <mqlzdfz@gmail.com>
2018-09-18 20:25:00 -0400

    trying to incode image

--- client.py
+++ client.py
@@ -7,15 +7,21 @@ import pickle
 import zlib
 from picamera.array import PiRGBArray
 from picamera import PiCamera
+
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(('10.186.44.62', 8485))
 connection = client_socket.makefile('wb')
 camera = PiCamera()
 rawCapture = PiRGBArray(camera)
-cam = cv2.VideoCapture(rawCapture)
+time.sleep(0.1)
+# grab an image from the camera
+camera.capture(rawCapture, format="bgr")
+image = rawCapture.array
 
-cam.set(3, 320)
-cam.set(4, 240)
+# cam = cv2.VideoCapture(rawCapture)
+
+# cam.set(3, 320)
+# cam.set(4, 240)
 
 img_counter = 0
 
@@ -23,7 +29,7 @@ encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
 
 while True:
     ret, frame = cam.read()
-    result, frame = cv2.imencode('.jpg', frame, encode_param)
+    result, frame = cv2.imencode('.jpg', image, encode_param)
 #    data = zlib.compress(pickle.dumps(frame, 0))
     data = pickle.dumps(frame, 0)
     size = len(data)
