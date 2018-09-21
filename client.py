14c0035d635d937222cbbec0cae19ea66e951fba
Qianli Ma <mqlzdfz@gmail.com>
2018-09-21 11:59:18 -0400

    test

--- client.py
+++ client.py
@@ -5,23 +5,28 @@ import struct
 import time
 import pickle
 import zlib
+from picamera.array import PiRGBArray
+from picamera import PiCamera
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(('127.0.0.1', 8485))
 connection = client_socket.makefile('wb')
+camera = PiCamera()
+rawCapture = PiRGBArray(camera)
+camera.capture(rawCapture, format="bgr")
+# cam = cv2.VideoCapture(0)
 
-cam = cv2.VideoCapture(0)
-
-cam.set(3, 320)
-cam.set(4, 240)
+# cam.set(3, 320)
+# cam.set(4, 240)
 
 img_counter = 0
 
 encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
 
 while True:
-    ret, frame = cam.read()
-    result, frame = cv2.imencode('.jpg', frame, encode_param)
+    # ret, frame = cam.read()
+    image = rawCapture.array
+    result, frame = cv2.imencode('.jpg', image, encode_param)
 #    data = zlib.compress(pickle.dumps(frame, 0))
     data = pickle.dumps(frame, 0)
     size = len(data)
