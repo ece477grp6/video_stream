c1425183f93887f75bdadd162494ea090c5eaf9f
Qianli Ma <mqlzdfz@gmail.com>
2018-09-21 21:41:19 -0400

    capture conrinous

--- client.py
+++ client.py
@@ -19,9 +19,19 @@ img_counter = 0
 
 encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
 
-while True:
-    # ret, frame = cam.read()
-    camera.capture(rawCapture, format="bgr")
+# while True:
+#     # ret, frame = cam.read()
+#     camera.capture(rawCapture, format="bgr")
+#     image = rawCapture.array
+#     result, frame = cv2.imencode('.jpg', image, encode_param)
+#     data = zlib.compress(pickle.dumps(frame, 0))
+#     data = pickle.dumps(frame, 0)
+#     size = len(data)
+
+#     print("{}: {}".format(img_counter, size))
+#     client_socket.sendall(struct.pack(">L", size) + data)
+#     img_counter += 1
+for camera.capture_continuous(rawCapture, format="bgr"):
     image = rawCapture.array
     result, frame = cv2.imencode('.jpg', image, encode_param)
     data = zlib.compress(pickle.dumps(frame, 0))
