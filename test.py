d83a8fda48cdeb94b23558bf3978e6cdfa94aaad
Qianli Ma <mqlzdfz@gmail.com>
2018-09-24 12:05:06 -0400

    fix

--- test.py
+++ test.py
@@ -43,22 +43,34 @@ import time
 
 class FPS:
     def __init__(self):
-        self.startTime = None
-        self.endTime = None
-        self.numFrames = 0
+        # store the start time, end time, and total number of frames
+        # that were examined between the start and end intervals
+        self._start = None
+        self._end = None
+        self._numFrames = 0
 
     def start(self):
-        self.startTime = datetime.now()
+        # start the timer
+        self._start = datetime.datetime.now()
+        return self
 
     def stop(self):
-        self.stopTime = datetime.now()
+        # stop the timer
+        self._end = datetime.datetime.now()
 
-    def updateFrameCount(self):
-        self.numFrames += 1
+    def update(self):
+        # increment the total number of frames examined during the
+        # start and end intervals
+        self._numFrames += 1
+
+    def elapsed(self):
+        # return the total number of seconds between the start and
+        # end interval
+        return (self._end - self._start).total_seconds()
 
     def fps(self):
-        fps = self.numFrames / (self.endTime - self.startTime).total_seconds()
-        return fps
+        # compute the (approximate) frames per second
+        return self._numFrames / self.elapsed()
 
 
 class PiVideoStream:
