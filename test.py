b7d83666fd531ffcff1a88461ec626052efe20a1
Qianli Ma <mqlzdfz@gmail.com>
2018-09-24 10:45:08 -0400

    update test for local fps

--- test.py
+++ test.py
@@ -5,29 +5,29 @@ import struct
 import time
 import pickle
 import zlib
+from picamera.array import PiYUVArray()
+from picamera import PiCamera
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('127.0.0.1', 8485))
-connection = client_socket.makefile('wb')
-
-cam = cv2.VideoCapture(0)
-
-cam.set(3, 320)
-cam.set(4, 240)
-
+# client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# client_socket.connect(('127.0.0.1', 8485))
+# connection = client_socket.makefile('wb')
 img_counter = 0
-
 encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+with PiCamera() as camera:
+    camera.resolution = (1280, 720)
+    camera.framerate = 24
+    rawCapture = PiRGBArray(camera, size=(1280, 720))
+    time.sleep(0.1)
 
-while True:
-    ret, frame = cam.read()
-    result, frame = cv2.imencode('.jpg', frame, encode_param)
-#    data = zlib.compress(pickle.dumps(frame, 0))
-    data = pickle.dumps(frame, 0)
-    size = len(data)
+    for foo in camera.capture_continuous(rawCapture, format="bgr"):
+        image = rawCapture.array
+        result, frame = cv2.imencode('.jpg', image, encode_param)
+        data = zlib.compress(pickle.dumps(frame, 0))
+        data = pickle.dumps(frame, 0)
+        size = len(data)
 
-    print("{}: {}".format(img_counter, size))
-    client_socket.sendall(struct.pack(">L", size) + data)
-    img_counter += 1
-
-cam.release()
+        print("{}: {}".format(img_counter, size))
+        # client_socket.sendall(struct.pack(">L", size) + data)
+        img_counter += 1
+        rawCapture.truncate(0)
+        cv2.imshow('ImageWindow', frame)
