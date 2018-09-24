062fb2e646768a9698e25dec09e1c8a87d8f035e
Qianli Ma <mqlzdfz@gmail.com>
2018-09-24 11:57:17 -0400

    not using imutils anymore

--- test.py
+++ test.py
@@ -1,104 +1,97 @@
-# # import cv2
-# # import io
-# # import socket
-# # import struct
-# # import time
-# # import pickle
-# # import zlib
-# # from picamera.array import PiRGBArray
-# # from picamera import PiCamera
-
-# # # client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# # # client_socket.connect(('127.0.0.1', 8485))
-# # # connection = client_socket.makefile('wb')
-# # img_counter = 0
-# # encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
-# # with PiCamera() as camera:
-# #     camera.resolution = (1280, 720)
-# #     camera.framerate = 24
-# #     rawCapture = PiRGBArray(camera, size=(1280, 720))
-# #     time.sleep(0.1)
-
-# #     for foo in camera.capture_continuous(rawCapture, format="bgr"):
-# #         image = rawCapture.array
-# #         result, frame = cv2.imencode('.jpg', image, encode_param)
-# #         data = zlib.compress(pickle.dumps(frame, 0))
-# #         data = pickle.dumps(frame, 0)
-# #         size = len(data)
-
-# #         print("{}: {}".format(img_counter, size))
-# #         # client_socket.sendall(struct.pack(">L", size) + data)
-# #         img_counter += 1
-# #         rawCapture.truncate(0)
-# #         cv2.imshow('ImageWindow', frame)
-# #         cv2.waitKey(1)
-# from datetime import datetime
-# from threading import Thread
 # import cv2
+# import io
+# import socket
+# import struct
+# import time
+# import pickle
+# import zlib
 # from picamera.array import PiRGBArray
 # from picamera import PiCamera
-# import argparse
 
+# # client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# # client_socket.connect(('127.0.0.1', 8485))
+# # connection = client_socket.makefile('wb')
+# img_counter = 0
+# encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+# with PiCamera() as camera:
+#     camera.resolution = (1280, 720)
+#     camera.framerate = 24
+#     rawCapture = PiRGBArray(camera, size=(1280, 720))
+#     time.sleep(0.1)
 
-# class FPS:
-#     def __init__(self):
-#         self.startTime = None
-#         self.endTime = None
-#         self.numFrames = 0
+#     for foo in camera.capture_continuous(rawCapture, format="bgr"):
+#         image = rawCapture.array
+#         result, frame = cv2.imencode('.jpg', image, encode_param)
+#         data = zlib.compress(pickle.dumps(frame, 0))
+#         data = pickle.dumps(frame, 0)
+#         size = len(data)
 
-#     def start(self):
-#         self.startTime = datetime.now()
-
-#     def stop(self):
-#         self.stopTime = datetime.now()
-
-#     def updateFrameCount(self):
-#         self.numFrames += 1
-
-#     def fps(self):
-#         fps = self.numFrames / (self.endTime - self.startTime).total_seconds()
-#         return fps
-
-
-# class PiVideoStream:
-#     def __init__(self, resolution=(1280, 720), framerate=30):
-#         self.camera = PiCamera()
-#         self.camera.resolution = resolution
-#         self.camera.framerate = framerate
-#         self.rawCapture = PiRGBArray(self.camera, size=resolution)
-#         self.stream = self.camera.capture_continuous(
-#             self.rawCapture, format="bgr")
-#         self.frame = None
-#         self.stopped = False
-
-#     def start(self):
-#         Thread(target=self.upgrade, args=()).start()
-#         return self
-
-#     def update(self):
-#         for frame in self.stream:
-#             self.frame = frame.array
-#             self.rawCapture.truncate(0)
-
-#             if self.stopped:
-#                 self.stream.close()
-#                 self.rawCapture.close()
-#                 self.camera.close()
-#                 return
-
-#     def read(self):
-#         return self.frame
-
-#     def stop(self):
-#         self.stopped = True
-from imutils.video.pivideostream import PiVideoStream
-from imutils.video import FPS
+#         print("{}: {}".format(img_counter, size))
+#         # client_socket.sendall(struct.pack(">L", size) + data)
+#         img_counter += 1
+#         rawCapture.truncate(0)
+#         cv2.imshow('ImageWindow', frame)
+#         cv2.waitKey(1)
+from datetime import datetime
+from threading import Thread
+import cv2
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import argparse
-import time
-import cv2
-import imutils
+
+
+class FPS:
+    def __init__(self):
+        self.startTime = None
+        self.endTime = None
+        self.numFrames = 0
+
+    def start(self):
+        self.startTime = datetime.now()
+
+    def stop(self):
+        self.stopTime = datetime.now()
+
+    def updateFrameCount(self):
+        self.numFrames += 1
+
+    def fps(self):
+        fps = self.numFrames / (self.endTime - self.startTime).total_seconds()
+        return fps
+
+
+class PiVideoStream:
+    def __init__(self, resolution=(1280, 720), framerate=30):
+        self.camera = PiCamera()
+        self.camera.resolution = resolution
+        self.camera.framerate = framerate
+        self.rawCapture = PiRGBArray(self.camera, size=resolution)
+        self.stream = self.camera.capture_continuous(
+            self.rawCapture, format="bgr")
+        self.frame = None
+        self.stopped = False
+
+    def start(self):
+        Thread(target=self.upgrade, args=()).start()
+        return self
+
+    def update(self):
+        for frame in self.stream:
+            self.frame = frame.array
+            self.rawCapture.truncate(0)
+
+            if self.stopped:
+                self.stream.close()
+                self.rawCapture.close()
+                self.camera.close()
+                return
+
+    def read(self):
+        return self.frame
+
+    def stop(self):
+        self.stopped = True
+
 
 arg = argparse.ArgumentParser()
 arg.add_argument("-n", "--num-frames", type=int, default=100,
@@ -120,7 +113,6 @@ for (i, f) in enumerate(stream):
     # grab the frame from the stream and resize it to have a maximum
     # width of 400 pixels
     frame = f.array
-    frame = imutils.resize(frame, width=400)
 
     # check to see if the frame should be displayed to our screen
     if args["display"] > 0:
@@ -158,7 +150,6 @@ while fps._numFrames < args["num_frames"]:
         # grab the frame from the threaded video stream and resize it
         # to have a maximum width of 400 pixels
     frame = vs.read()
-    frame = imutils.resize(frame, width=400)
 
     # check to see if the frame should be displayed to our screen
     if args["display"] > 0:
